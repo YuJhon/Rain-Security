0ee47b3199065cfbad03d9e1ed17317eaccfad4c
YuJhon <3291371805@qq.com>
2017-10-19 19:07:00 +0800

    默认跳转的登录页面的配置修改

--- rain-security-core/src/main/java/com/jhon/rain/security/core/properties/BrowserProperties.java
+++ rain-security-core/src/main/java/com/jhon/rain/security/core/properties/BrowserProperties.java
@@ -18,8 +18,9 @@ public class BrowserProperties {
 	/** 注册页面 **/
 	private String signUpUrl = "/rain-signUp.html";
 
-	/** 默认为未授权的url **/
-	private String loginPage = SecurityConstants.DEFAULT_UNAUTHENTICATION_URL;
+	/** 默认为未授权的url SecurityConstants.DEFAULT_UNAUTHENTICATION_URL**/
+	private String loginPage = SecurityConstants.DEFAULT_LOGIN_PAGE_URL;
+
 	/** 返回值类型枚举 **/
 	private LoginResponseTypeEnums loginType = LoginResponseTypeEnums.JSON;
 	/** 记住我的时间:一小时 **/
